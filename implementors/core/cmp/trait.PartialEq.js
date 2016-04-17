eeb2e1a26c2475de3364b27b98b0dcd18d9eb282
Kevin Brightwell <kevin.brightwell2@gmail.com>
2016-04-17 16:43:32 -0400

    Documentation!

---
+++ implementors/core/cmp/trait.PartialEq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['crypto'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='crypto/argon2/enum.Variant.html' title='crypto::argon2::Variant'>Variant</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='struct' href='crypto/argon2/struct.Algorithm.html' title='crypto::argon2::Algorithm'>Algorithm</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='struct' href='crypto/argon2/struct.HashBldr.html' title='crypto::argon2::HashBldr'>HashBldr</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='enum' href='crypto/argon2/enum.ParamErr.html' title='crypto::argon2::ParamErr'>ParamErr</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='struct' href='crypto/curve25519/struct.Fe.html' title='crypto::curve25519::Fe'>Fe</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.PartialEq.html' title='core::cmp::PartialEq'>PartialEq</a> for <a class='struct' href='crypto/mac/struct.MacResult.html' title='crypto::mac::MacResult'>MacResult</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['crypto'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='enum' href='crypto/argon2/enum.Variant.html' title='crypto::argon2::Variant'>Variant</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='crypto/argon2/struct.HashBldr.html' title='crypto::argon2::HashBldr'>HashBldr</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='crypto/argon2/struct.Algorithm.html' title='crypto::argon2::Algorithm'>Algorithm</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
